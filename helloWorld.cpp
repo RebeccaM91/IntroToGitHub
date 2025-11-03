08c9cd1351f2a5b6b504fcc6e4f163e80300403f
Rebecca Michaud <rmichaud02478@gmail.com>
2025-11-03 13:00:00 -0700

    helloWorld

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello, World!" << endl;
+    cout << 10 / 20 << endl;
+    string x = "r";
+    string y = "t";
+    string z = x + "1" + y + "2";
+    cout << z << endl;
+    return 0;
+}
